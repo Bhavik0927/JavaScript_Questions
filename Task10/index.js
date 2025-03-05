c68fdcb7dc1470369a3038464c0664f6e1655272
Bhavik <bhaviktembhare2@gmail.com>
2025-03-05 07:44:14 +0530

    custom reducer

--- Task10/index.js
+++ Task10/index.js
@@ -31,3 +31,20 @@ const numbers = [1, 2, 3, 4, 5];
 const evens = customFilter(numbers, (element) => element % 2 === 0);
 console.log('Even numbers:', evens);
 
+
+
+// Custom Reduce function
+
+const customReduce = (arr,fn,init) =>{
+    let val = init;
+    for(let i = 0; i< arr.length; i++){
+        val = fn(val, arr[i]);
+    }
+    return val;
+}
+
+const nums = [1, 2, 3, 4,53];
+const sumFn = (acc, curr) => acc + curr;
+const init = 0;
+
+console.log(customReduce(nums, sumFn, init)); 
