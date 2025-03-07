169b619d1e5dccf910d1095aa98638112bba9b91
Bhavik <bhaviktembhare2@gmail.com>
2025-03-07 10:42:15 +0530

    reverse string

--- DSA/String.js
+++ DSA/String.js
@@ -22,4 +22,41 @@ const removeParanthesis = (str) => {
 
 }
 
-console.log(removeParanthesis("(()())(())"));
+// console.log(removeParanthesis("(()())(())"));
+
+// Reverse String;
+const reverseString = (str) =>{
+    return str.split(' ').reverse().join(' ');
+}
+
+// console.log(reverseString("If the world is ending "));
+
+// Reverse By Two Pointers
+const reverseStringTwoPointers = (str) =>{
+    const chars = str.split('');
+    let start = 0;
+
+    const reverse = (start,end) =>{
+        while(start < end){
+            const temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    reverse(0,chars.length - 1);
+
+    for(let i = 0; i< chars.length; i++){
+        if(chars[i] === ' '){
+            reverse(start, i - 1);
+            start = i + 1;
+        }
+    }
+
+    reverse(start, chars.length - 1);
+    return chars.join('');
+}
+
+console.log(reverseStringTwoPointers("If the world is ending "));
