967eea49b6fed2ba2c40b5c33e5d4f19167c8542
Bhavik <bhaviktembhare2@gmail.com>
2025-03-11 09:24:48 +0530

    find missing number

--- DSA/Array.js
+++ DSA/Array.js
@@ -81,5 +81,38 @@ const maxSubArraySum = (arr) =>{
     return totalSum;
 }
 
-console.log(maxSubArraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+// console.log(maxSubArraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+
+// Brute force approach 
+// Time Complexity = O(n^2)  
+const findMissingNum = (arr,N) =>{
+    for(let Num = 1; Num < N; Num++){
+        let found = false;
+        for(let i = 0; i< arr.length; i++){
+            if(arr[i] === Num){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            return Num;
+        }
+    }
+}
+
+// console.log(findMissingNum([1,2,3,5,6,7,8,10], 9))
+
+
+// Optimal approach
+const findMissingNumOptimal = (arr,N) =>{
+    const totalSum = (N * (N+1))/2;
+    const currentSum = arr.reduce((acc,curr) => acc+ curr,0);
+    const diff = totalSum - currentSum;
+    return diff;
+}
+
+console.log(findMissingNumOptimal([1,2,3,5], 5))
+
+
 
