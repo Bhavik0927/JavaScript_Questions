9c7364c3a518830c4974f8ed7112da037d44f60c
Bhavik <bhaviktembhare2@gmail.com>
2025-03-29 09:27:25 +0530

    hamming Distance

--- DSA/String.js
+++ DSA/String.js
@@ -67,7 +67,7 @@ const reverseStringTwoPointers = (str) => {
 const permuteString = (str) => {
     let result = [];
 
-    const swap = (arr,i, j) => {
+    const swap = (arr, i, j) => {
         let temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -80,13 +80,13 @@ const permuteString = (str) => {
 
 
         for (let i = left; i < arr.length; i++) {
-            swap(arr,i,left);
+            swap(arr, i, left);
             permute(arr, left + 1);
-            swap(arr, i,left);
+            swap(arr, i, left);
         }
     }
 
-    permute(str.split(""),0);
+    permute(str.split(""), 0);
     return result;
 
 }
@@ -98,10 +98,10 @@ const permuteString = (str) => {
 // String str = "Hello world!"; maxLength = 4;
 // Output: "Hell..."
 
-const truncate = (str,maxLength) =>{
-    if(str.length >= maxLength){
-        return str.slice(0,maxLength) + "...";
-    }else return str;
+const truncate = (str, maxLength) => {
+    if (str.length >= maxLength) {
+        return str.slice(0, maxLength) + "...";
+    } else return str;
 }
 
 // console.log(truncate("Helloooooooooo world!",3)); // Hell...
@@ -109,8 +109,60 @@ const truncate = (str,maxLength) =>{
 
 // Is Palindrome Number
 
-const isPalindrome = (num) =>{
-    return num === Number(num.toString().split("").reverse().join("")) ;
+const isPalindrome = (num) => {
+    return num === Number(num.toString().split("").reverse().join(""));
 }
 
- console.log(isPalindrome(901)); // true      
+//  console.log(isPalindrome(901)); // true 
+
+
+// Hamming Distance
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+// input1 = "hello";
+// input2 = "hrlle";
+// Output: 2;
+
+
+// 1st implementation
+const hammingDistance = (str1, str2) => {
+    if (str1.length !== str2.length) return -1;
+
+    let count = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] !== str2[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(hammingDistance("hello", "hrlle")); // 2
+
+// 2nd Implementation
+const hammingDistance2 = (str1, str2) => {
+    str1 = str1.toString(2);
+    str2 = str2.toString(2);
+
+    if (str1.length < str2.length) {
+        while (str1.length !== str2.length) {
+            str1 = "0" + str1;
+            
+
+        }
+    } else {
+        while (str1.length !== str2.length) {
+            str2 = "0" + str2;
+            
+        }
+    }
+
+    let count = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] !== str2[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(hammingDistance2(3, 5))
