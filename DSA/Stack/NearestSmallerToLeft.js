4fbcb8d5d62ec6123737960ec0c9ad678e080f14
Bhavik <bhaviktembhare2@gmail.com>
2025-05-10 23:22:09 +0530

    Nearest Smaller To Left

---
+++ DSA/Stack/NearestSmallerToLeft.js
@@ -0,0 +1,20 @@
+const NearestSmallerToLeft = (arr) =>{
+    let stack = [];
+    let vector = [];
+
+    for(let i = 0; i < arr.length; i++){
+        while(stack.length && stack[stack.length - 1] >= arr[i]){
+            stack.pop();
+        }
+        if(stack.length === 0){
+            vector.push(-1);
+        }else{
+            vector.push(stack[stack.length -1])
+        }
+        stack.push(arr[i]);
+    }
+    return vector;
+}
+
+const arr = [4,5,2,10,8];
+console.log(NearestSmallerToLeft(arr));
